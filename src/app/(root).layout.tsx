764d72cb0fbdc5a24ba68ecd11470cce1da75ec7
HoiLen <len1231.hoi@gmail.com>
2025-03-11 16:40:09 +0900

    ルートのヘッダーコンポーネントにpropsを埋め込みました

--- src/app/(root).layout.tsx
+++ src/app/(root).layout.tsx
@@ -1,12 +1,19 @@
 import { Footer } from "../components/Footer";
 import Header from "../components/Header";
+import pageName from "./@pageName/(root).page";
 import "./global.css";
-export default function RootLayout({ children }: React.PropsWithChildren<{}>) {
+
+export default function RootLayout({
+  pageName,
+  children,
+}: React.PropsWithChildren<{
+  pageName: React.ReactNode
+}>) {
   return (
     <html lang="ja">
       <body>
         <div className="flex flex-col min-h-screen">
-          <Header />
+          <Header pageName={pageName}/>
           <main className="flex-grow">{children}</main>
           <Footer />
         </div>
