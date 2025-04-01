3060a29cadcdf3f4ba54c2b2c76df97031d74319
HoiLen <len1231.hoi@gmail.com>
2025-04-01 10:34:21 +0900

    ビュー数順でソートされた記事を取得する関数を追加しました。

--- convex/articles.ts
+++ convex/articles.ts
@@ -1,16 +1,48 @@
+import { v } from "convex/values";
 import { query } from "./_generated/server";
 
+// 記事の更新順で記事を取得
 export const get = query({
   // 引数
   args: {},
 
-  // 実際の処理部分
+  // 実際の処理の部分
   handler: async (ctx) => {
     const articles = await ctx.db.query("articles").collect();
 
     const sortedArticles = articles.sort(
-      (a, b) => b._creationTime - a._creationTime
+      (a, b) => b._creationTime - a._creationTime // 降順ソート
     );
+
+    return sortedArticles.map((article) => {
+      return {
+        id: article._id,
+        title: article.title,
+        description: article.description,
+        author: article.author,
+        createdAt: article._creationTime,
+        viewCount: article.viewCount,
+      };
+    });
+  },
+});
+
+// 記事のビュー数の多い順で記事を取得
+export const getPopular = query({
+  // 引数
+  args: {
+    // convex の型指定 (JSのnumber型と互換性のある convex の型を v から持ってきている)
+    limit: v.optional(v.number()),
+  },
+
+  // 実際の処理の部分
+  handler: async (ctx, args) => {
+    const { limit } = args;
+    const articles = await ctx.db.query("articles").collect();
+
+    const sortedArticles = articles
+      .sort((a, b) => b.viewCount - a.viewCount) // 降順ソート
+      .slice(0, limit ?? 10); // 取得した記事の個数の上限を決める。デフォルトでは 10
 
     return sortedArticles.map((article) => {
       return {
