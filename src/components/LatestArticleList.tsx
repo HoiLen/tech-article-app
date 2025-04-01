1d2047fa902f762b71d79a9f4a47bf3df3d63db2
HoiLen <len1231.hoi@gmail.com>
2025-04-01 10:02:00 +0900

    コメントを追記しました

--- src/components/LatestArticleList.tsx
+++ src/components/LatestArticleList.tsx
@@ -7,12 +7,15 @@ import { Article } from "../domain/Article";
 import ArticleCard from "./ArticleCard";
 
 const LatestArticleList = () => {
+  // 記事情報の取得
   const articlesData = useQuery(api.articles.get);
   const [articles, setArticles] = useState<Article[]>([]);
 
+  // articlesData に更新が入ったら実行
   useEffect(() => {
     if (!articlesData) return;
 
+    // articlesData から１つずつ記事情報を取り出して Article型に変換して articles に渡す
     const articles = articlesData?.map((a) => {
       return new Article(
         a.id,
@@ -23,11 +26,12 @@ const LatestArticleList = () => {
         a.viewCount
       );
     });
+    // state の articles にセット
     setArticles(articles);
   }, [articlesData]);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="ml-5 mr-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {articles.map((article) => (
         <span key={article.id}>
           <ArticleCard article={article} />
