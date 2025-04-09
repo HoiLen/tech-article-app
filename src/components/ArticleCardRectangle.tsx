40776468b9e3c723d9db5df7a9d44b5772481fbd
HoiLen <len1231.hoi@gmail.com>
2025-04-09 14:10:46 +0900

    別パターンの記事カードを作成しました

---
+++ src/components/ArticleCardRectangle.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { FC } from "react";
+import { Article } from "../domain/Article";
+import { Link } from "@lazarv/react-server/navigation";
+import { Eye } from "lucide-react";
+
+type Props = {
+  article: Article;
+};
+
+const ArticleCardRectangle: FC<Props> = ({ article }) => {
+  return (
+    <Link to={`/articles/${article.id}`} className="w-[30%]">
+      <div
+        className="
+          border
+          border-gray-300
+          rounded-lg overflow-hidden
+          hover:bg-gray-50
+          hover:shadow-xl
+          transition duration-300
+          cursor-pointer
+          h-auto
+          mb-2"
+      >
+        <div className="h-2 bg-orange-300"></div>
+        <div className="p-6 h-auto flex flex-col mb-1">
+          <h3 className="text-lg font-bold text-orange-950">{article.title}</h3>
+
+          {/* icon / author / publish day / view count */}
+          <div className="flex items-center mt-2">
+            <div className="w-6 h-6 flex rounded-full bg-teal-100 items-center justify-center font-medium">
+              {article.author.charAt(0)}
+            </div>
+            {/* <div> */}
+            <div className="ml-2 font-medium">{article.author}</div>
+            <div className="ml-2 text-gray-500">
+              {new Date(article.createdAt).toLocaleDateString()}
+            </div>
+            <Eye className="ml-2 w-4 h-4 text-gray-500" />
+            <div className="ml-1 text-gray-500">{article.viewCount}</div>
+            {/* </div> */}
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default ArticleCardRectangle;
